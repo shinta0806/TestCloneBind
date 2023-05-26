ca44d444dae26195cf9cfb2eaad57c1751a5363b
shinta0806 <shinta.0806@gmail.com>
2023-05-27 00:19:10 +0900

    インクリメント実装。

---
+++ TestCloneBind/Models/ValueContainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TestCloneBind.Models;
+
+public class ValueContainer : ObservableObject
+{
+    private Int32 _value;
+    public Int32 Value
+    {
+        get => _value;
+        set => SetProperty(ref _value, value);
+    }
+}
--- TestCloneBind/ViewModels/MainViewModel.cs
+++ TestCloneBind/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using TestCloneBind.Models;
 
 namespace TestCloneBind.ViewModels;
 
@@ -6,5 +8,22 @@ public partial class MainViewModel : ObservableRecipient
 {
     public MainViewModel()
     {
+        ButtonIncrementClickedCommand = new RelayCommand(ButtonIncrementClicked);
+    }
+
+    public ValueContainer ValueContainer
+    {
+        get;
+        set;
+    } = new();
+
+    public RelayCommand ButtonIncrementClickedCommand
+    {
+        get;
+    }
+
+    private void ButtonIncrementClicked()
+    {
+        ValueContainer.Value++;
     }
 }
